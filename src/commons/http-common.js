e35d9dd1b8d56524532915856f02e2be49b79811
danxdbsx <danxdbsx@gmail.com>
2021-11-10 13:37:49 +0800

    Revert "11/10 with js"
    This reverts commit 9d0764d35185f424d38fc042f8a5b34e6241368e.

--- src/commons/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'http://localhost:8080/api/v1/greet',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
--- src/services/helloService.js
+++
@@ -1,8 +0,0 @@
-import httpCommon from "../commons/http-common";
-
-const getHello = () =>{
-    
-    return httpCommon.get('/hello'); 
-}
-
-export default {getHello};
